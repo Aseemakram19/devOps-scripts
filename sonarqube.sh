4a2f377124afcfaea0cc97a7e5caa9e7cd884c93
MOHD ASEEM AKRAM <113539818+Aseemakram19@users.noreply.github.com>
2024-08-04 08:49:11 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,3 +1,12 @@
-# INSTALL SONARQUBE
-  
-    docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+# 1. install docker & INSTALL SONARQUBE AS A CONTAINER IN THE EC2 INSTANCE
+#Cloud Aseem Auhor
+sudo apt-get update -y
+sudo apt-get install docker.io -y
+sleep 5
+sudo usermod -aG docker ubuntu 
+sudo usermod -aG docker jenkins 
+newgrp docker
+sleep 5
+sudo chmod 777 /var/run/docker.sock
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+sleep 5
